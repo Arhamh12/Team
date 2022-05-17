660a17c75ad01e3639cad27a36ccd4e3e262f713
Arhamh12 <erhamashamsi@gmail.com>
2022-05-17 16:18:14 +1000

    some changes here and there

---
+++ javaQuestions.java
@@ -0,0 +1,12 @@
+/**
+ * javaQuestions
+ */
+public class javaQuestions {
+    public static void main(String[] args) {
+        String myWords = "I am the witch ";
+        int myNumbers = 13;
+        System.out.print(myWords + myNumbers);
+    }
+}
+
+    
