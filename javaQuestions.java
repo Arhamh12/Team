aaf705083e9b905790efb7a12b4de03487124b61
Arhamh12 <erhamashamsi@gmail.com>
2022-05-17 18:24:01 +1000

    finished Save the witch

--- javaQuestions.java
+++ javaQuestions.java
@@ -3,10 +3,23 @@
  */
 public class javaQuestions {
     public static void main(String[] args) {
-        String myWords = "I am the witch ";
-        int myNumbers = 13;
-        System.out.print(myWords + myNumbers);
+        String myString = "How dare you?";
+        int i;
+        int[] myArray = new int[13];
+        if (myString.charAt(2) == 'w'){
+            for ( i = 0; i < myString.length(); i++) {
+                myArray[i] = i;
+                System.out.print(myArray[i]);
+            }
+        }
+        else{
+            System.out.print("Third letter is not w");
+        }
+            
+        
     }
 }
+
+
 
     
